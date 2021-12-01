25773a2a738fc431dafd4eb93d2e3bf239be8500
Ilya Daraseliya <idarase@gmail.com>
2021-12-01 21:17:52 +0100

    feat: cache manager

--- apps/mtl-server/src/app/app.module.ts
+++ apps/mtl-server/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 import { UserController } from '../user/user.controller';
 import { UserService } from '../user/user.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register()],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService, PrismaService],
+  providers: [
+    AppService,
+    UserService,
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
