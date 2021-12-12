8c4b840b303871010897db4c61cf68a68d9b0819
Ilya Daraseliya <idarase@gmail.com>
2021-12-12 18:42:18 +0100

    fix: screenshot

--- apps/mtl-server/src/screenshot/screenshot.controller.ts
+++ apps/mtl-server/src/screenshot/screenshot.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query, Res } from '@nestjs/common';
 import { Response } from '../types/Response';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
+import { processErrorResponse } from '../utils/error';
 
 @Controller()
 export class ScreenshotController {
@@ -9,13 +10,17 @@ export class ScreenshotController {
 
   @Get('screenshot')
   async screenshot(@Res() res: Response, @Query('query') query: string) {
-    const response = await axios.request<ArrayBuffer>({
-      url: `${this.configService.get(
-        'app.screenshotBaseUrl'
-      )}/api/screenshot?${query}`,
-      responseType: 'arraybuffer',
-    });
+    try {
+      const response = await axios.request<ArrayBuffer>({
+        url: `${this.configService.get(
+          'app.screenshotBaseUrl'
+        )}/api/screenshot?${query}`,
+        responseType: 'arraybuffer',
+      });
 
-    res.status(200).send(Buffer.from(new Uint8Array(response.data)));
+      res.status(200).send(Buffer.from(new Uint8Array(response.data)));
+    } catch (error) {
+      res.status(300).send(processErrorResponse(error));
+    }
   }
 }
