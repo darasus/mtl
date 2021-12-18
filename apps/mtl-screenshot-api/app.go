98d305452de3a48be3fbe6defd64562b509dd5dd
Ilya Daraseliya <idarase@gmail.com>
2021-12-18 14:01:35 +0100

    fix: clean up mtl screenshot

--- apps/mtl-screenshot-api/app.go
+++ apps/mtl-screenshot-api/app.go
@@ -12,11 +12,8 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var postId = r.URL.Query().Get("id")
-	println(postId)
 	var url = "https://www.mytinylibrary.com/p/" + postId + "/preview"
-	println(url)
 	var fileBytes = Screenshot(url)
-
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(fileBytes)
@@ -24,31 +21,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Screenshot(urlstr string) []byte {
-	// create context
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
 		// chromedp.WithDebugf(log.Printf),
 	)
 	defer cancel()
-
-	// capture screenshot of an element
 	var buf []byte
-
-	// capture entire browser viewport, returning png with quality=90
 	if err := chromedp.Run(ctx, fullScreenshot(urlstr, 100, &buf)); err != nil {
 		log.Fatal(err)
 	}
-
 	return buf
 }
 
 func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
-	println(urlstr)
-	println(quality)
-	println(res)
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
-		//chromedp.EmulateScale(2)
 		chromedp.EmulateViewport(1200, 630, chromedp.EmulateScale(2)),
 		chromedp.WaitVisible(`#logo`, chromedp.ByID),
 		chromedp.FullScreenshot(res, quality),
