116da9ae38a91cd5286682e4b740d314a3bf99ba
Ilya Daraseliya <idarase@gmail.com>
2021-12-18 20:08:01 +0100

    test

--- apps/mtl-server/src/auth/jwt.strategy.ts
+++ apps/mtl-server/src/auth/jwt.strategy.ts
@@ -7,6 +7,20 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(BaseStrategy) {
   constructor(configService: ConfigService) {
+    console.log({
+      AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+      AUTH0_AUTH_MANAGER_AUDIENCE: process.env.AUTH0_AUTH_MANAGER_AUDIENCE,
+      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+      AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+      DATABASE_URL: process.env.DATABASE_URL,
+      PUSHER_APP_CLUSTER: process.env.PUSHER_APP_CLUSTER,
+      PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+      PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
+      PUSHER_APP_SECRET: process.env.PUSHER_APP_SECRET,
+      SCREENSHOT_API_BASE_URL: process.env.SCREENSHOT_API_BASE_URL,
+    });
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
